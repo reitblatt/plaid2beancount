6863567e8ded7fa341d14924f6918a63ff472236
Mark Reitblatt <mark@reitblatt.com>
2025-11-01 14:07:18 -0700

    rm old django code

--- transactions/forms.py
+++
@@ -1,8 +0,0 @@
-# forms.py
-from django import forms
-from .models import Account
-
-class TransactionFilterForm(forms.Form):
-    account = forms.ModelChoiceField(queryset=Account.objects.all())
-    start_date = forms.DateField(required=False, widget=forms.DateInput(attrs={'type': 'date'}))
-    end_date = forms.DateField(required=False, widget=forms.DateInput(attrs={'type': 'date'}))
